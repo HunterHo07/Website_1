de70edf3d761301b3e02cc33b26a1e4d5cf704e6
HunterHo07 <goldcool07@gmail.com>
2023-05-22 08:56:55 +0800

    Updates

---
+++ precache-manifest.710662f481c84f32307bdd50cdf0f0aa.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "df6d7c298ad4bcbd10d8",
+    "url": "/Website_1/static/css/main.301558fc.chunk.css"
+  },
+  {
+    "revision": "df6d7c298ad4bcbd10d8",
+    "url": "/Website_1/static/js/main.df6d7c29.chunk.js"
+  },
+  {
+    "revision": "cf03a93b689970f7f2c2",
+    "url": "/Website_1/static/js/1.cf03a93b.chunk.js"
+  },
+  {
+    "revision": "302af07cb1bd159d40d4",
+    "url": "/Website_1/static/js/runtime~main.302af07c.js"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/Website_1/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/Website_1/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/Website_1/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/Website_1/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/Website_1/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/Website_1/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "ab44f346e4b0678f9a40a0d8452d8a4d",
+    "url": "/Website_1/static/media/ai.ab44f346.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/Website_1/static/media/possibility.3c84ae26.png"
+  },
+  {
+    "revision": "cf50a52dbe0370b7cb2dc2d4cf4af173",
+    "url": "/Website_1/index.html"
+  }
+];
